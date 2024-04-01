e0a1d5968d1634bb2ba68345802fbc60d5c0e2e2
Luiz Mesquita <luiz.mesquita@pivot88.com>
2024-04-01 18:48:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+## OpenAI Console Chat
+
+Este repositório contém um único arquivo com um script PHP para interagir com a API da OpenAI e realizar conversas de texto. O script permite que você faça perguntas e receba respostas geradas pelo modelo de linguagem da OpenAI.
+
+### Pré-requisitos
+
+Antes de executar o script, você precisa ter o PHP 7.4 ou superior instalado em seu sistema. Você pode verificar se o PHP está instalado executando o seguinte comando no terminal:
+```bash
+php -v
+```
+
+Você também pode instalar o PHP no linux usando 
+```bash
+sudo apt get instal php
+```
+
+### Configuração
+Para executar o script, você também precisa de uma chave de API da OpenAI. Se você ainda não tem uma chave de API, pode criá-la em [https://platform.openai.com/api-keys](https://platform.openai.com/api-keys).
+
+O script verifica se a chave da API da OpenAI está definida como uma variável de ambiente. Se não estiver definida, solicitará que você insira a chave da API e a adicionará ao arquivo de configuração do shell para persistência.
+
+O script é compatível com os shells `bash` e `zsh`. Ele determinará automaticamente o arquivo de configuração correto com base no shell atual do usuário.
+
+### Como usar
+
+Crie o link simbólico para o script no diretório `/usr/local/bin`:
+```bash
+sudo ln -s "$(pwd)/gpt.php" /usr/local/bin/gpt
+```
+
+Em seguida, torne o script executável e atualize o shell para reconhecer o novo comando:
+```bash
+sudo chmod +x gpt.php
+source ~/.bashrc
+```
+
+Agora você pode executar o script globalmente no terminal digitando `gpt` e passando os argumentos necessários.
+```bash
+#Exemplo:
+gpt "qual a capital do Brasil?"
+```
+
+### Limitações
+
+Este script é uma implementação básica para interagir com a API da OpenAI e pode ter algumas limitações. Certifique-se de revisar a documentação da OpenAI para obter informações detalhadas sobre como usar a API e quaisquer limitações ou restrições aplicáveis.
+
+### Contribuições
+
+Se você encontrar problemas ou tiver sugestões de melhoria, sinta-se à vontade para abrir uma issue ou enviar uma solicitação de pull request.
+
+### Licença
+
+Este projeto é livre para cópia e distribuição
---
+++ gpt.php
@@ -0,0 +1,104 @@
+#!/usr/bin/php
+
+<?php
+// sudo wget -O /usr/local/bin/script.sh https://gist.githubusercontent.com/luizalbertobm/f9331f25211732752e77e7065b72acca/raw/6d6769d2d8f6a0a1f4b447c0f45b0aff6780568c/
+// sudo chmod +x /usr/local/bin/*
+
+// Nome da variável de ambiente
+$envVarName = 'OPENAI_API_KEY'; //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
+
+// Verifica se a variável de ambiente já está definida
+if (getenv($envVarName) === false) {
+    echo "A chave da API da OpenAI não está definida.\nVocê pode criar uma chave em https://platform.openai.com/api-keys.\nPor favor, insira a chave da API: ";
+    $handle = fopen ("php://stdin","r");
+    $apiKey = trim(fgets($handle));
+
+    if (!empty($apiKey)) {
+        // Determina o arquivo de configuração do shell com base no shell atual
+        $shellName = basename($_SERVER['SHELL']);
+        $shellConfigFile = $_SERVER['HOME'] . '/.'.$shellName.'rc';
+
+        switch (basename($_SERVER['SHELL'])) {
+            case 'bash':
+                $shellConfigFile = $_SERVER['HOME'] . '/.bashrc';
+                break;
+            case 'zsh':
+                $shellConfigFile = $_SERVER['HOME'] . '/.zshrc';
+                break;
+            // Adicione mais casos conforme necessário para outros shells
+            default:
+                echo "Não foi possível determinar o arquivo de configuração do shell atual.";
+                exit;
+        }
+
+        // Adiciona a variável de ambiente ao arquivo de configuração
+        file_put_contents($shellConfigFile, "\nexport $envVarName=\"$apiKey\"\n", FILE_APPEND);
+
+        // Informa ao usuário que a chave foi adicionada e instruções para recarregar o arquivo de configuração
+        echo "A chave da API foi adicionada ao seu $shellConfigFile. Por favor, reinicie o terminal ou execute o comando 'source $shellConfigFile' para aplicar as mudanças.\n";
+        exit;
+    } else {
+        echo "Chave da API inválida. Não é possível executar o comando.\n";
+        exit;
+    }
+}
+
+// Check if the user has provided a command line parameter
+if ($argc < 2) {
+    echo 'Por favor façca uma pergunta. Por exemplo `gpt "Qual a capital da Fraça"`';
+    exit(1);
+}
+
+// Your OpenAI API key
+$apiKey = getenv($envVarName);
+
+// Request Header
+$headers = [
+    'Authorization: Bearer ' . $apiKey,
+    'Content-Type: application/json'
+];
+
+// Data to be sent in the request
+$data = [
+    // 'model' => 'gpt-4-1106-preview',
+    'model' => 'gpt-3.5-turbo',
+    'messages' => [
+        [
+            'role' => 'user',
+            'content' => $argv[1],
+        ]
+    ],
+    'temperature' => 0.7,
+    'max_tokens' => 600  // Adjust the number of tokens as needed
+];
+
+// API endpoint for OpenAI (you may need to modify this based on the specific API you are using)
+$apiUrl = 'https://api.openai.com/v1/chat/completions';
+
+// Initialize cURL session
+$ch = curl_init($apiUrl);
+
+// Set cURL options
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Execute the POST request
+$response = curl_exec($ch);
+
+// Close cURL session
+curl_close($ch);
+
+// Decode and display the response
+$responseData = json_decode($response, true);
+echo "=== \n";
+if(isset($responseData['error'])) {
+    echo "Erro: " . $responseData['error']['message'] . "\n";
+    exit(1);
+}
+
+foreach ($responseData['choices'] as $choice) {
+    echo $choice['message']['content'] . "\n";
+    exit(0);
+}
