c8c5c49761a14c8b3e6bbd05fec46d2dbd962225
Luiz Mesquita <luizalbertobm@gmail.com>
2024-05-01 13:48:36 +0100

    chore(install.sh): Update installation script with new version and instructions
    Updated installation script to remove the old version and install the new version of 'gpt'. Added instructions for users to open a new terminal or update the current terminal after installation.

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
-# remove the old gpt file
+# Remove the old version
 sudo rm /usr/local/bin/gpt
 
-# Copy the gpt.php file to the /usr/bin directory renaming it to only gpt
+# install the new version
 sudo cp gpt.php /usr/local/bin/gpt
 sudo chmod +x /usr/local/bin/gpt
 
 # Check if the gpt file exists in the /usr/bin directory
 if [ -f /usr/local/bin/gpt ]; then
     echo "GPT has been installed successfully"
+    echo "You have to open a new terminal or update the current terminal to use the script"
 else
     echo "GPT has not been installed"
 fi
-echo "You have to open a new terminal or update the current terminal to use the script"
