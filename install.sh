b4f3fa4a8ac69ff85b987ec172e47f1e193cd83a
Luiz Mesquita <luizalbertobm@gmail.com>
2024-05-01 04:35:22 +0100

    feat: add check for gpt file existence in install script

--- install.sh
+++ install.sh
@@ -7,5 +7,10 @@ sudo rm /usr/local/bin/gpt
 sudo cp gpt.php /usr/local/bin/gpt
 sudo chmod +x /usr/local/bin/gpt
 
-echo "GPT has been installed successfully"
+# Check if the gpt file exists in the /usr/bin directory
+if [ -f /usr/local/bin/gpt ]; then
+    echo "GPT has been installed successfully"
+else
+    echo "GPT has not been installed"
+fi
 echo "You have to open a new terminal or update the current terminal to use the script"
